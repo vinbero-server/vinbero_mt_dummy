c9124993315044becc70cc472fbef1d3290dd9ca
Byeonggon Lee <gonny952@gmail.com>
2018-05-30 03:53:51 +0900

    Commit

--- src/vinbero_mt_dummy.c
+++ src/vinbero_mt_dummy.c
@@ -9,9 +9,9 @@
 #include <vinbero_common/vinbero_common_Config.h>
 #include <vinbero_common/vinbero_common_Log.h>
 #include <vinbero_common/vinbero_common_Module.h>
-#include <vinbero/vinbero_IModule.h>
-#include <vinbero/vinbero_ITLocal.h>
-#include <vinbero/vinbero_ITlService.h>
+#include <vinbero/vinbero_IMODULE.h>
+#include <vinbero/vinbero_ITLOCAL.h>
+#include <vinbero/vinbero_ITLSERVICE.h>
 
 struct vinbero_mt_dummy_LocalModule {
     const char* message;
@@ -22,11 +22,11 @@ VINBERO_IMODULE_FUNCTIONS;
 VINBERO_ITLOCAL_FUNCTIONS;
 VINBERO_ITLSERVICE_FUNCTIONS;
 
-int vinbero_IModule_init(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
-    VINBERO_COMMON_LOG_TRACE("in %s()", __FUNCTION__);
+int vinbero_IMODULE_init(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
+    VINBERO_COMMON_LOG_TRACE2();
     module->name = "vinbero_mt_dummy";
     module->version = "0.0.1";
-    module->needsChildren = false;
+    module->childrenRequired = false;
     module->localModule.pointer = malloc(1 * sizeof(struct vinbero_mt_dummy_LocalModule));
     struct vinbero_mt_dummy_LocalModule* localModule = module->localModule.pointer;
     vinbero_common_Config_getString(config, module, "vinbero_mt_dummy.message", &(localModule->message), "I HAVE NOTHING TO SAY");
@@ -34,50 +34,49 @@ int vinbero_IModule_init(struct vinbero_common_Module* module, struct vinbero_co
     return 0;
 }
 
-int vinbero_IModule_rInit(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
-    VINBERO_COMMON_LOG_TRACE("in %s()", __FUNCTION__);
+int vinbero_IMODULE_rInit(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
+    VINBERO_COMMON_LOG_TRACE2();
     return 0;
 }
 
-int vinbero_ITLocal_init(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
-    VINBERO_COMMON_LOG_TRACE("in %s()", __FUNCTION__);
+int vinbero_ITLOCAL_init(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
+    VINBERO_COMMON_LOG_TRACE2();
     return 0;
 }
 
-int vinbero_ITLocal_rInit(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
-    VINBERO_COMMON_LOG_TRACE("in %s()", __FUNCTION__);
+int vinbero_ITLOCAL_rInit(struct vinbero_common_Module* module, struct vinbero_common_Config* config, void* args[]) {
+    VINBERO_COMMON_LOG_TRACE2();
     return 0;
 }
 
-int vinbero_ITlService_call(struct vinbero_common_Module* module, void* args[]) {
-    VINBERO_COMMON_LOG_TRACE("in %s()", __FUNCTION__);
+int vinbero_ITLSERVICE_call(struct vinbero_common_Module* module, void* args[]) {
+    VINBERO_COMMON_LOG_TRACE2();
     struct vinbero_mt_dummy_LocalModule* localModule = module->localModule.pointer;
     while(true) {
-        printf("%s\n", localModule->message);
+        VINBERO_COMMON_LOG_DEBUG("%s", localModule->message);
         sleep(localModule->interval);
     }
     return 0;
 }
 
-int vinbero_ITLocal_destroy(struct vinbero_common_Module* module) {
-    VINBERO_COMMON_LOG_TRACE("in %s()", __FUNCTION__);
+int vinbero_ITLOCAL_destroy(struct vinbero_common_Module* module) {
+    VINBERO_COMMON_LOG_TRACE2();
     return 0;
 }
 
-int vinbero_ITLocal_rDestroy(struct vinbero_common_Module* module) {
-VINBERO_COMMON_LOG_TRACE("%s: %u: %s", __FILE__, __LINE__, __FUNCTION__);
+int vinbero_ITLOCAL_rDestroy(struct vinbero_common_Module* module) {
+    VINBERO_COMMON_LOG_TRACE2();
     return 0;
 }
 
-int vinbero_IModule_destroy(struct vinbero_common_Module* module) {
-VINBERO_COMMON_LOG_TRACE("%s: %u: %s", __FILE__, __LINE__, __FUNCTION__);
+int vinbero_IMODULE_destroy(struct vinbero_common_Module* module) {
+    VINBERO_COMMON_LOG_TRACE2();
     return 0;
 }
 
-int vinbero_IModule_rDestroy(struct vinbero_common_Module* module) {
-VINBERO_COMMON_LOG_TRACE("%s: %u: %s", __FILE__, __LINE__, __FUNCTION__);
-struct vinbero_mt_dummy_LocalModule* localModule = module->localModule.pointer;
-    VINBERO_COMMON_LOG_TRACE("%s: %u: %s", __FILE__, __LINE__, __FUNCTION__);
+int vinbero_IMODULE_rDestroy(struct vinbero_common_Module* module) {
+    VINBERO_COMMON_LOG_TRACE2();
+    struct vinbero_mt_dummy_LocalModule* localModule = module->localModule.pointer;
     free(module->localModule.pointer);
     if(module->tlModuleKey != NULL)
         free(module->tlModuleKey);
